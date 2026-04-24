e7435680841255c141edff8d36fd2483cfcbaf33
SaurabhJadli <saurabhjadli7@protonmail.com>
2026-04-24 19:43:41 +0530

    font setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ function App() {
 
   return (
     <>
+      <form action="">
+        <label htmlFor="name" className=''>Name: </label>
+        <input type="text" name="name" />
 
+        <label htmlFor="email">Email: </label>
+        <input type="email" name="email" />
+
+        <label htmlFor="file">File: </label>
+        <input type="file" name="file" />
+
+        <button type="submit">Submit</button>
+      </form>
     </>
   )
 }
